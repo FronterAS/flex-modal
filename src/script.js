c799de8dfd50f9a8eed06cba5aa654a37bd26fa7
Gavin King <gavin.king@pearson.com>
2015-03-18 16:46:25 +0100

    Initial commit

---
+++ src/script.js
@@ -0,0 +1,14 @@
+(function () {
+    'use strict';
+
+    var calcHeight = function () {
+        // we can't just grab the height of the flex-item as it is totally whack in IE10 and 11
+        var h = $('#modal').height() - $('#header').height();
+
+        $('.directive').height(h);
+    };
+
+    $(function () {
+        calcHeight();
+    });
+}(jQuery));
